963fe834a28396c3b113fe227da297105056e19f
SMOLKEYS <jiroshimonis@gmail.com>
2022-01-04 20:09:03 +0800

    diechecks

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,14 @@ Events.on(ContentInitEvent, () => {
     Timer.schedule(() => {
         Vars.content.blocks().each(b => {
             if(b instanceof LogicBlock) b.buildVisibility = BuildVisibility.hidden;
-            return;
             
             if(b instanceof LogicDisplay) b.buildVisibilty = BuildVisibility.hidden;
-            return;
         });
         
         Groups.build.each(b => {
             if(b instanceof LogicBlock) b.kill();
-            return;
             
             if(b instanceof LogicDisplay) b.kill();
-            return;
         });
     }, 3, 5, -1);
 });
